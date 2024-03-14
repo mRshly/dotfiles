aa67ccfaa629841a478e3033c23faa81054677d8
mushroom-stanley <mshrm808@icloud.com>
2024-03-14 13:15:19 +0900

    fix(fish): move abbreviations to abbr.fish from config.fish

---
+++ .config/fish/abbr.fish
@@ -0,0 +1,25 @@
+# nvim
+abbr --add vim nvim
+
+## docker
+abbr --add dc docker compose
+
+## git
+abbr --add g git
+abbr --add ga git add
+abbr --add gb git branch
+abbr --add gc git commit
+abbr --add gd git diff
+abbr --add gl git log
+abbr --add gp git push origin
+abbr --add gcb git checkout -b
+abbr --add gcz git cz
+abbr --add gsh git show
+abbr --add gst git status
+abbr --add gsw git switch
+
+## eza
+abbr --add la eza --icons --git -al
+abbr --add ll eza --icons --git -l
+abbr --add lar eza --icons --time-style relative --git -al
+abbr --add llr eza --icons --time-style relative --git -l
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -55,32 +55,7 @@ function fish_user_key_bindings
 end
 
 # abbreviations
-
-## nvim
-abbr --add vim nvim
-
-## docker
-abbr --add dc docker compose
-
-## git
-abbr --add g git
-abbr --add ga git add
-abbr --add gb git branch
-abbr --add gc git commit
-abbr --add gd git diff
-abbr --add gl git log
-abbr --add gp git push origin
-abbr --add gcb git checkout -b
-abbr --add gcz git cz
-abbr --add gsh git show
-abbr --add gst git status
-abbr --add gsw git switch
-
-## eza
-abbr --add la eza --icons --git -al
-abbr --add ll eza --icons --git -l
-abbr --add lar eza --icons --time-style relative --git -al
-abbr --add llr eza --icons --time-style relative --git -l
+source (dirname (status --current-filename))/abbr.fish
 
 # brew
 fish_add_path /usr/local/sbin
