67a33af8e8bc939817bf5c3e0ec0e3de6e3ffeaf
mushroom-stanley <mshrm808@icloud.com>
2024-03-23 23:45:29 +0900

    feat(nvim): add nvim-cmp for input emoji

--- config/nvim/lua/plugins/config.lua
+++ config/nvim/lua/plugins/config.lua
@@ -4,4 +4,12 @@ return {
     cmd = "IncRename",
     config = true,
   },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
 }
