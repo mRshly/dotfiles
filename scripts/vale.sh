a190402ee232862d575410232fbadeb3d486bdd0
mRshly <mshrm808@icloud.com>
2024-03-26 04:33:50 +0900

    feat(vale): add Microsoft style

--- scripts/vale.sh
+++ scripts/vale.sh
@@ -7,6 +7,7 @@ set -o pipefail
 # This script is used to setup Vale after it has been installed.
 
 CONFIG_DIR="${HOME}/.config/vale"
+DOTFILE_DIR="${HOME}/dotfiles/config/vale"
 
 if [ ! -d "${CONFIG_DIR}" ]; then
 	echo "Config directory does not exist. Exiting..."
@@ -15,5 +16,13 @@ fi
 
 echo config directory is "${CONFIG_DIR:?}"
 
+# Link Vale configuration files
+echo Begin linking Vale configuration files
+ln -s "${DOTFILE_DIR}/.vale.ini" "${CONFIG_DIR:?}/.vale.ini"
+ln -s "${DOTFILE_DIR}/styles" "${CONFIG_DIR:?}/styles"
+echo End linking Vale configuration files
+
+# Sync Vale styles
+echo Begin linkng Vale styles
 cd "${CONFIG_DIR:?}"
 vale sync
