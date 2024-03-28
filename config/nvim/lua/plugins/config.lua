e444efd7ecfb0d2bdb6ba4cc240162a37699ceef
mRshly <mshrm808@icloud.com>
2024-03-29 02:05:50 +0900

    fix(nvim): remove IncRename plugin: Sometimes stop neovim

--- config/nvim/lua/plugins/config.lua
+++ config/nvim/lua/plugins/config.lua
@@ -1,10 +1,5 @@
 return {
   {
-    "smjonas/inc-rename.nvim",
-    cmd = "IncRename",
-    config = true,
-  },
-  {
     "hrsh7th/nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji" },
     ---@param opts cmp.ConfigSchema
