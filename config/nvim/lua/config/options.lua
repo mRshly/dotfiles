640ffa1f1281a239dd07df2a6891fd9b8875999b
mRshly <mshrm808@icloud.com>
2024-03-29 23:22:30 +0900

    fix(nvim): add config molten-nvim loads kernel in virtualenv

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -16,3 +16,5 @@ package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/shar
 -- TODO: set those paths when just open python file.
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.local/share/mise/installs/python/latest/bin/python;"
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/config/python/.venv/bin/python;"
+
+vim.g.python3_host_prog = vim.fn.expand("$HOME/.config/python/.venv/bin/python3")
