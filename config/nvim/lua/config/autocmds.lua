8423cb3e67a7882b486c1524526b39872c0d46ff
mRshly <mshrm808@icloud.com>
2024-04-04 12:10:32 +0900

    fix(nvim): replace autocmd for python from fredrikaverpil/dotfiles to linux-cultist/venv-selector.nvim

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -6,29 +6,15 @@
 -- see: https://github.com/LazyVim/LazyVim/discussions/392
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
--- Python virtualenv
-local function find_python_executable()
-  if vim.env.VIRTUAL_ENV then
-    local paths = vim.fn.glob(vim.env.VIRTUAL_ENV .. "/**/bin/python", true, true)
-    local executable_path = table.concat(paths, ", ")
-    if executable_path ~= "" then
-      vim.api.nvim_echo({ { "Using path for python: " .. executable_path, "None" } }, false, {})
-      return executable_path
-    end
-  elseif vim.fn.filereadable(".venv/bin/python") == 1 then
-    local executable_path = vim.fn.expand(".venv/bin/python")
-    vim.api.nvim_echo({ { "Using path for python: " .. executable_path, "None" } }, false, {})
-    return executable_path
-  end
-  vim.api.nvim_echo({ { "No python executable found (see autocmds.lua)", "WarningMsg" } }, false, {})
-end
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "python" },
+-- from: https://github.com/linux-cultist/venv-selector.nvim?tab=readme-ov-file#-automate
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Auto select virtualenv Nvim open",
+  pattern = "*",
   callback = function()
-    vim.g.python3_host_prog = find_python_executable() -- python executable
-    vim.opt_local.colorcolumn = "72,88" -- Ruler at column number
-    vim.opt_local.tabstop = 4 -- Number of spaces tabs count for
-    vim.opt_local.shiftwidth = 4 -- Size of an indent
+    local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+    if venv ~= "" then
+      require("venv-selector").retrieve_from_cache()
+    end
   end,
+  once = true,
 })
