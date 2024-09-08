28445068e302be2a29440af1de3ed7e55158a2d0
mRshly <mshrm808@icloud.com>
2024-09-08 23:45:50 +0900

    fix(nvim): remove unused local variable

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,24 +1,3 @@
-local function prefer_bin_from_venv(executable_name)
-  -- Return the path to the executable if $VIRTUAL_ENV is set and the binary exists somewhere beneath the $VIRTUAL_ENV path, otherwise get it from Mason
-  if vim.env.VIRTUAL_ENV then
-    local paths = vim.fn.glob(vim.env.VIRTUAL_ENV .. "/**/bin/" .. executable_name, true, true)
-    local executable_path = table.concat(paths, ", ")
-    if executable_path ~= "" then
-      -- vim.api.nvim_echo(
-      -- 	{ { "Using path for " .. executable_name .. ": " .. executable_path, "None" } },
-      -- 	false,
-      -- 	{}
-      -- )
-      return executable_path
-    end
-  end
-
-  local mason_registry = require("mason-registry")
-  local mason_path = mason_registry.get_package(executable_name):get_install_path() .. "/venv/bin/" .. executable_name
-  -- vim.api.nvim_echo({ { "Using path for " .. executable_name .. ": " .. mason_path, "None" } }, false, {})
-  return mason_path
-end
-
 return {
   {
     "williamboman/mason.nvim",
