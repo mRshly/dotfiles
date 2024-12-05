d6643652434bcf636b608097a9a9bc446decab45
mRshly <mshrm808@icloud.com>
2024-12-06 03:02:18 +0900

    fix(fish): remove bobthefish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,46 +1,4 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set -g theme_display_git yes
-    set -g theme_display_git_dirty yes
-    set -g theme_display_git_untracked yes
-    set -g theme_display_git_ahead_verbose yes
-    set -g theme_display_git_dirty_verbose yes
-    set -g theme_display_git_stashed_verbose yes
-    set -g theme_display_git_default_branch yes
-    set -g theme_git_default_branches develop
-    set -g theme_git_worktree_support yes
-    set -g theme_use_abbreviated_branch_name no
-    set -g theme_display_vagrant no
-    set -g theme_display_docker_machine yes
-    set -g theme_display_k8s_context yes
-    set -g theme_display_hg yes
-    set -g theme_display_virtualenv yes
-    set -g theme_display_nix no
-    set -g theme_display_ruby no
-    set -g theme_display_go verbose
-    set -g theme_display_node yes
-    set -g theme_display_user ssh
-    set -g theme_display_hostname ssh
-    set -g theme_display_vi yes
-    set -g theme_display_date yes
-    set -g theme_display_cmd_duration yes
-    # Title options
-    set -g theme_title_display_process yes
-    set -g theme_title_display_path no
-    set -g theme_title_display_user no
-    set -g theme_title_use_abbreviated_path no
-    set -g theme_display_user yes
-    set -g theme_date_timezone Asia/Tokyo
-    set -g theme_avoid_ambiguous_glyphs yes
-    set -g theme_powerline_fonts yes
-    set -g theme_nerd_fonts yes
-    set -g theme_show_exit_status yes
-    set -g theme_display_jobs_verbose yes
-    set -g theme_color_scheme terminal-dark
-    set -g fish_prompt_pwd_dir_length 1
-    set -g theme_project_dir_length 1
-    set -g theme_newline_cursor yes
-    set -g theme_newline_prompt '> '
     # see https://github.com/wez/wezterm/issues/415#issuecomment-755849623
     set -x TERM wezterm
     set -gx EDITOR nvim
