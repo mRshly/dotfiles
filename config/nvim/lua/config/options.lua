9276c4f43c85ac15b842acec867347d0bb84aca1
mRshly <mshrm808@icloud.com>
2025-01-27 01:00:42 +0900

    fix(nvim): change column size 120 to 100

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -10,7 +10,10 @@ vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
 -- Python path
 -- TODO: set those paths when just open python file.
-package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.local/share/mise/installs/python/latest/bin/python;"
+package.path = package.path
+  .. ";"
+  .. vim.fn.expand("$HOME")
+  .. "/.local/share/mise/installs/python/latest/bin/python;"
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.config/python/.venv/bin/python;"
 
 vim.g.python3_host_prog = vim.fn.expand("$HOME/.config/python/.venv/bin/python3")
--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -89,7 +89,10 @@ return {
           local label = {}
 
           for severity, icon in pairs(icons) do
-            local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+            local n = #vim.diagnostic.get(
+              props.buf,
+              { severity = vim.diagnostic.severity[string.upper(severity)] }
+            )
             if n > 0 then
               table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
             end
