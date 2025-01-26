a7febfeefe15abd33f5ce497e93f5daad69bdecb
mRshly <mshrm808@icloud.com>
2025-01-26 23:53:24 +0900

    fix(wezterm): change font to adjust new display

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -10,11 +10,11 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 config.color_scheme = "Solarized Dark (Gogh)"
 
-config.font = wezterm.font("HackGen35 Console NF")
-config.font_size = 16.0
+config.font = wezterm.font("HackGen Console NF")
+config.font_size = 18.0
 config.window_frame = {
-	font = wezterm.font({ family = "HackGen35 Console NF", weight = "Bold" }),
-	font_size = 12.0,
+	font = wezterm.font({ family = "HackGen Console NF", weight = "Bold" }),
+	font_size = 14.0,
 }
 
 -- Window config
