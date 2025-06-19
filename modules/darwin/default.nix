f63d611181b47fdb5dd38b49872701c586c9a882
mRshly <mshrm808@icloud.com>
2025-06-20 02:47:09 +0900

    fix(nix): uninstall homemanager

--- modules/darwin/default.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{
-  home.homeDirectory = "/Users/mrshly";
-
-  programs.fish.shellInit = ''
-    fish_add_path -a ~/Applications/Ghostty.app/Contents/MacOS/
-    fish_add_path -a /opt/homebrew/bin/
-  '';
-}
