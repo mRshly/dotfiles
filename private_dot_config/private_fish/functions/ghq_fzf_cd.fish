c9d5f9b01def83c198143d8cefbe9b0b79281f94
mRshly <mshrm808@icloud.com>
2026-02-26 21:26:10 +0900

    fix(fish): ghq_fzf_cd ensure copy path by <C-y>

--- private_dot_config/private_fish/functions/ghq_fzf_cd.fish
+++ private_dot_config/private_fish/functions/ghq_fzf_cd.fish
@@ -1,5 +1,9 @@
 function ghq_fzf_cd -d "Move to ghq managed repo"
-    set -l repo (ghq list -p | fzf --reverse --height 80% --border --preview 'set -l f (fd -d 1 -i "^README.*" {} | head -n 1); if test -n "$f"; bat --color=always --style=header,grid --line-range :200 "$f"; else; eza --color=always --icons --tree --level 1 {}; end' </dev/tty)
+    set -l repo (ghq list -p | fzf --reverse --height 80% --border \
+        --preview 'set -l f (fd -d 1 -i "^README.*" {} | head -n 1); if test -n "$f"; bat --color=always --style=header,grid --line-range :200 "$f"; else; eza --color=always --icons --tree --level 1 {}; end' \
+        --bind 'ctrl-y:execute-silent(echo {} | pbcopy)+abort' \
+        --header 'Enter: cd, Ctrl-y: copy path' \
+        </dev/tty)
     if test -n "$repo"
         cd "$repo"
     end
