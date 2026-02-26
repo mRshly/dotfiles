14fdd89f7c8b5992c9309e6cb91171f45446e7ad
mRshly <mshrm808@icloud.com>
2026-02-26 22:03:18 +0900

    feat(fish): add preview scrolling to ghq_fzf_cd

--- private_dot_config/private_fish/functions/ghq_fzf_cd.fish
+++ private_dot_config/private_fish/functions/ghq_fzf_cd.fish
@@ -1,7 +1,8 @@
 function ghq_fzf_cd -d "Move to ghq managed repo"
     set -l out (ghq list -p | fzf --reverse --height 80% --border \
         --preview 'set -l f (fd -d 1 -i "^README.*" {} | head -n 1); if test -n "$f"; CLICOLOR_FORCE=1 glow -s dark -w $FZF_PREVIEW_COLUMNS "$f"; else; eza --color=always --icons --tree --level 1 {}; end' \
-        --header 'Enter: cd, Ctrl-y: copy path' \
+        --header 'Enter: cd, Ctrl-y: copy path, PgUp/PgDn: scroll preview' \
+        --bind 'pgup:preview-up,pgdn:preview-down' \
         --expect=ctrl-y \
         </dev/tty)
 
