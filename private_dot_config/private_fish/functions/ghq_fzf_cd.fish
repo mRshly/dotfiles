6c1af900c2551aa6b7756b01a502c3e0a00656da
mRshly <mshrm808@icloud.com>
2026-02-26 21:55:17 +0900

    feat(fish): modernize ghq_fzf_cd with glow preview and path copying

--- private_dot_config/private_fish/functions/ghq_fzf_cd.fish
+++ private_dot_config/private_fish/functions/ghq_fzf_cd.fish
@@ -1,11 +1,19 @@
 function ghq_fzf_cd -d "Move to ghq managed repo"
-    set -l repo (ghq list -p | fzf --reverse --height 80% --border \
-        --preview 'set -l f (fd -d 1 -i "^README.*" {} | head -n 1); if test -n "$f"; bat --color=always --style=header,grid --line-range :200 "$f"; else; eza --color=always --icons --tree --level 1 {}; end' \
-        --bind 'ctrl-y:execute-silent(echo {} | pbcopy)+abort' \
+    set -l out (ghq list -p | fzf --reverse --height 80% --border \
+        --preview 'set -l f (fd -d 1 -i "^README.*" {} | head -n 1); if test -n "$f"; CLICOLOR_FORCE=1 glow -s dark -w $FZF_PREVIEW_COLUMNS "$f"; else; eza --color=always --icons --tree --level 1 {}; end' \
         --header 'Enter: cd, Ctrl-y: copy path' \
+        --expect=ctrl-y \
         </dev/tty)
+
+    set -l key $out[1]
+    set -l repo $out[2]
+
     if test -n "$repo"
-        cd "$repo"
+        if test "$key" = "ctrl-y"
+            echo "$repo" | pbcopy
+        else
+            cd "$repo"
+        end
     end
     commandline -f repaint
 end
